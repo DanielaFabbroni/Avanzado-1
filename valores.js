ec0d0f83b151366cce939fcb6e73489afdf1cae6
Daniela Fabbroni <danielafabbroni7@gmail.com>
2022-09-12 12:55:25 -0300

    aprendiendo basicos

---
+++ valores.js
@@ -0,0 +1,5 @@
+40
+"Diego de granada"
+
+[1,2,3,4];
+{nombre: "Diego"};
---
+++ variables.js
@@ -0,0 +1,22 @@
+var nombre = "Daniela";
+nombre
+//Anotaciones
+var edad;
+edad = 48;
+edad
+var marcasCelu = ["Lg", "Samsung"];
+
+marcasCelu
+
+var persona = { nombre: "Diego", edad: 30};
+persona
+var comidas = {
+    sanas: "verduras",
+    peso: "bajar",
+    grasas: 0
+};
+comidas
+console.log (comidas.peso);
+
+
+
